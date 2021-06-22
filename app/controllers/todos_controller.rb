e564ad976161aa71da66daaa65474cce0fa1b50d
masoda1112 <wendy.odakura@gmail.com>
2021-06-22 16:30:16 +0900

    締め切りまでの時間によるメッセージ出しわけ機能実装

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -12,18 +12,15 @@ require 'date'
   def create
     todo = Todo.create(name: params[:name], expiration_time: params[:expiration_time], is_finished: false)
     tag_id = params[:tags_id]
-
     tag_id.map do |t|
       TodoTag.create(todo_id: todo.id, tag_id: t)
     end
-
     response = {
       "id": todo.id,
       "name": todo.name,
       "expiration_time": todo.expiration_time,
       "error_message": todo.errors.full_messages
     }
-
     render json: response
     # todoに紐づくtagも追加
   end
@@ -35,16 +32,7 @@ require 'date'
   end
 
   def search
-    # new_todos = Todo.joins(todo_tags: :tag).select(" todos.id, todos.name, label")
     todos = Todo.where(name: params[:name])
-    # new_todos_2 = todos.map do |t|
-    #   tags = new_todos.where(id: t.id)
-    #   {
-    #     "id": t.id,
-    #     "name": t.name,
-    #     "label": tags.pluck(:label)
-    #   }
-    # end
     new_todos = build_todo_response(todos)
     render json: new_todos
 
@@ -61,18 +49,20 @@ require 'date'
   # private以下は外部から呼び出せない
   private
   def update_is_expired(todos)
-    just_time = DateTime.new
+    just_time = Time.at(Time.now.to_i)
     todos.map do |t|
       # 仮で入れておく
-      #  todo_extension = t.expiration_time - just_time;
-       todo_extension = 10
+       todo_extension = (Time.at(t.expiration_time) - just_time).to_i;
+      #  todo_extension = 10
         if todo_extension <= 0 && t.is_finished == false then
          t.is_expired == true
         end
     end
+    return todos
   end
 
   def build_todo_response(todos)
+    just_time = Time.at(Time.new.to_i)
     new_todos = todos.map do |t|
       labels = t.tags.pluck(:label)
       expiration_message = build_expiration_message(t.expiration_time)
@@ -81,22 +71,43 @@ require 'date'
         "name": t.name,
         "label": labels,
         "expiration_message": expiration_message,
+        "expriration_time": t.expiration_time,
+        "just_time": just_time,
         "is_finished": t.is_finished,
       }
     end
   end
 
   def build_expiration_message(expiration_time)
-    just_time = DateTime.new
-      # 仮で入れておく
-      #  todo_extension = t.expiration_time - just_time;
-      todo_extension = 10
+    just_time = Time.at(Time.now.to_i)
+    # 仮で入れておく
+    todo_year_extension = (expiration_time.year - just_time.year).to_i;
+    todo_month_extension = (expiration_time.month - just_time.month).to_i;
+    todo_day_extension = (expiration_time.day - just_time.day).to_i;
+    todo_hour_extension = (expiration_time.hour - just_time.hour).to_i;
+    todo_min_extension = (Time.at(expiration_time).min - just_time.min).to_i;
+
+    # todo_extension = 10
     expirationMessage = 
-      if todo_extension <= 1 then
+      if todo_year_extension = 0 &&
+         todo_mounth_extension = 0 && 
+         todo_day_extension = 0 && 
+         todo_hour_extension = 0 && todo_min_extension <= 1 && 
+         todo_min_extension >= 0 then
         ("まもなくタスクの終了期限です！")
-      elsif todo_extension <= 10 then
+      elsif todo_year_extension = 0 && 
+        todo_mounth_extension = 0 && 
+        todo_day_extension = 0 && 
+        todo_hour_extension = 0 && 
+        todo_min_extension <= 10 && 
+        todo_min_extension > 1 then
         ("残り10分です！")
-      elsif todo_extension <= 60 then
+      elsif todo_year_extension = 0 &&
+         todo_mounth_extension = 0 && 
+         todo_day_extension = 0 && 
+         todo_hour_extension = 0 && 
+         todo_min_extension <= 60 && 
+         todo_min_extension > 10 then
         ("終了まで１時間を切りました！")
       else
         ("時間はあります！焦らないでやりましょう！")
