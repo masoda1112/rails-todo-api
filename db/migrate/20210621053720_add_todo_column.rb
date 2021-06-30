c163f56cb1a0c027e36b7be3b3b44cf0e5da1c38
masoda1112 <wendy.odakura@gmail.com>
2021-06-30 21:41:32 +0900

    migrateファイル一部修正

--- db/migrate/20210621053720_add_todo_column.rb
+++ db/migrate/20210621053720_add_todo_column.rb
@@ -1,5 +1,5 @@
 class AddTodoColumn < ActiveRecord::Migration[6.1]
   def change
-    add_column :todos, :expiration_time ,　:datetime
+    add_column :todos, :expiration_time , :datetime
   end
 end
